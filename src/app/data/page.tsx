364419c1216f28a1f86ce4b331a97cc8655d011c
Bishnu Basyal <imvsnu@gmail.com>
2025-06-04 15:13:29 +0545

    feat: populate table with api data

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -1,56 +1,48 @@
 'use client';
 
+import _ from 'lodash';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchData } from './slice';
+import { fetchData, Product } from './slice';
 import { RootState, AppDispatch } from '@/lib/store';
 
 import { Table, TableColumn } from '@/components/Table';
 
-type User = {
-  id: number;
-  name: string;
-  role: 'Admin' | 'Editor' | 'Viewer';
-};
-
-const users: User[] = [
-  { id: 1, name: 'Alice', role: 'Admin' },
-  { id: 2, name: 'Bob', role: 'Editor' },
-  { id: 3, name: 'Charlie', role: 'Viewer' },
-  { id: 4, name: 'Diana', role: 'Admin' },
-  { id: 5, name: 'Eve', role: 'Viewer' },
-  { id: 6, name: 'Frank', role: 'Editor' },
-];
-
-const columns: TableColumn<User>[] = [
+const columns: TableColumn<Product>[] = [
   { key: 'id', label: 'ID', width: 'w-16' },
-  { key: 'name', label: 'Name', width: 'w-[200px]' },
-  { key: 'role', label: 'Role', width: 'w-32' },
+  { key: 'title', label: 'Title', width: 'w-16' },
+  { key: 'category', label: 'Category', width: 'w-16' },
+  { key: 'price', label: 'Price', width: 'w-16' },
+  { key: 'rating', label: 'Rating', width: 'w-16' },
+  { key: 'brand', label: 'Brand', width: 'w-16' },
+  { key: 'thumbnail', label: 'Thumbnail', width: 'w-16' },
 ];
-
 
 export default function DataPage() {
   const dispatch = useDispatch<AppDispatch>();
   const { data, loading, error } = useSelector((state: RootState) => state.data);
 
-  console.log('Data:', data);
-  console.log('Loading:', loading);
-  console.log('Error:', error);
-
   useEffect(() => {
     dispatch(fetchData());
   }, [dispatch]);
 
+  if (loading) {
+    return <div className="text-center">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="text-center text-red-500">{error}</div>;
+  }
+
   return (
     <div className="bg-white rounded-lg shadow p-6">
       <h1 className="text-2xl font-semibold mb-4">Data</h1>
-
       <Table
-        data={users}
+        data={data.products || []}
         columns={columns}
-        searchableKey="name"
-        filterOptions={{ key: 'role', values: ['Admin', 'Editor', 'Viewer'] }}
-        pageSize={3}
+        searchableKey="title"
+        filterOptions={{ key: 'category', values: _.uniq(data.products?.map((item: Product) => item.category) ?? []) }}
+        pageSize={10}
       />
     </div>
   )
